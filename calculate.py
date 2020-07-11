0a0fbaf4f365329c04edfbcaeb5a5f0e64e99153
Student-02-Mike <66924226+Student-02-Mike@users.noreply.github.com>
2020-07-11 17:46:18 +0100

    Update calculate.py

--- calculate.py
+++ calculate.py
@@ -4,3 +4,30 @@ import math
 def add(a, b):
     return a + b
 
+
+def subtract(a, b):
+    return a - b
+
+
+def multiply(a, b):
+    return a * b
+
+
+def divide(a, b):
+    return a / b
+
+
+def reminder(a, b):
+    return a % b
+
+
+def square(a):
+    return a * a
+
+
+def ceil(a):
+    return 0
+
+
+def floor(a):
+    return 0
